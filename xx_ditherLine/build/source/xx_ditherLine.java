931a822569a9f4ec315b642de969207f7e199f2b
qantip <641995+qantip@users.noreply.github.com>
2019-09-30 13:36:12 +0200

    Add files via upload

--- xx_ditherLine/build/source/xx_ditherLine.java
+++ xx_ditherLine/build/source/xx_ditherLine.java
@@ -15,10 +15,10 @@ import java.io.IOException;
 public class xx_ditherLine extends PApplet {
 
 int WIDTH, HEIGHT;
-float ANGLE = 35;
+float ANGLE = 35; // works only on angles 0° - 90°
 float SPACING = 7;
 float length = 600;
-String imgPath = "lukas.jpg";
+String imgPath = "img2.jpg";
 PImage img;
 
 public void settings(){
@@ -30,60 +30,41 @@ public void settings(){
 
 public void setup() {
   background(0);
-  //background(#253a52);
+  background(0xff070033);
   float xSpacing = SPACING / sin(radians(ANGLE));
   float ySpacing = SPACING / cos(radians(ANGLE));
   print (xSpacing, ySpacing);
   float x,y,x1,y1,angleToCorner;
   // X for cycle
-  y = 0;
+  y = (cos(radians(ANGLE)))>0 ? 0 : height;
   for(x = 0; x < width; x += xSpacing){
     angleToCorner = degrees(atan((height - y)/(width - x)));
     if (angleToCorner < ANGLE){
       // yMax
       x1 = x + (1/(tan(radians(ANGLE)))*(height - y));
       y1 = height;
-      //x1 = x + cos(radians(ANGLE))*length;
-      //y1 = y + sin(radians(ANGLE))*length;
+
     } else {
       // xMax
       x1 = width;
       y1 = y + tan(radians(ANGLE))*(width - x);
     }
 
-    /**
-    line(x,y,x1,y1);
-    fill(#ff0000);
-    ellipse(x,y,5,5);
-    fill(#00ff00);
-    ellipse(x1,y1,5,5);
-    **/
-
     widthLine(x,y,x1,y1);
   }
   // Y for cycle
-  x = 0;
+  x = (sin(radians(ANGLE)))>0 ? 0 : width;
   for(y = ySpacing; y < height; y += ySpacing){
     angleToCorner = degrees(atan((height - y)/(width - x)));
     if (angleToCorner < ANGLE){
       // yMax
       x1 = x + (1/(tan(radians(ANGLE)))*(height - y));
       y1 = height;
-      //x1 = x + cos(radians(ANGLE))*length;
-      //y1 = y + sin(radians(ANGLE))*length;
     } else {
       // xMax
       x1 = width;
       y1 = y + tan(radians(ANGLE))*(width - x);
     }
-
-    /**
-    line(x,y,x1,y1);
-    fill(#ff0000);
-    ellipse(x,y,5,5);
-    fill(#00ff00);
-    ellipse(x1,y1,5,5);
-    **/
 
     widthLine(x,y,x1,y1);
   }
@@ -102,26 +83,19 @@ public void widthLine(float x,float y,float x1,float y1){
     int c = img.get(PApplet.parseInt(x2),PApplet.parseInt(y2));
     float w = map(brightness(c),0,255,wMin,wMax);
     noStroke();
-    colorMode(HSB);
-    fill(0,0,255);
+    //colorMode(HSB);
+    fill(0xfffffee8);
+    //fill(0,0,255);
     //fill(hue(c),saturation(c)/2,255);
-    fill(0,0,255,PApplet.parseInt(map(brightness(c),0,255,0,125)));
+    //fill(0,0,255,int(map(brightness(c),0,255,0,125)));
     ellipse(x2,y2,w,w);
   }
 
-
-  /**
-  line(x,y,x1,y1);
-  fill(#ff0000);
-  ellipse(x,y,5,5);
-  fill(#00ff00);
-  ellipse(x1,y1,5,5);
-  **/
   saveFrame(imgPath+"_result.jpg");
 }
 
 public void draw(){
-
+  noLoop();
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "xx_ditherLine" };
